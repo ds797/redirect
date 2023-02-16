47d55e3f9e795dfc4f4762f1d44c0a214d37418c
ds797 <73960745+ds797@users.noreply.github.com>
2023-02-15 21:34:58 -0800

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	window.location.replace('https://ds-advance.pages.dev');
+</script>
+
+<main>
+	<h1>Redirecting...</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
